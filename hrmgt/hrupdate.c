a110188e6aa2352dca4933fb025586ca151babfd
Alex Meng <mengxp@inspur.com>
2024-02-28 12:23:03 +0800

    update hrupdate
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- hrmgt/hrupdate.c
+++ hrmgt/hrupdate.c
@@ -11,7 +11,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
 #ifndef MD5_DIGEST_LENGTH
 #define MD5_DIGEST_LENGTH 16
 #endif
@@ -20,6 +19,13 @@
 #define UPGRADE_FILE_TAG BAD_CAST "UpgradeFile"
 #define DOWNLAOD_TYPE_TAG BAD_CAST "DownloadType"
 #define UPGRADESOFTWARE_TAG BAD_CAST "UpgradeSoftware"
+
+#define UPGRADE_REPORT_CODE_DOWNLOADING 100
+#define UPGRADE_REPORT_CODE_DOWNLOAD_SUCCESS 101
+#define UPGRADE_REPORT_CODE_DOWNLOAD_FAILED 102
+#define UPGRADE_REPORT_CODE_UPGRADE_SUCCESS 103
+#define UPGRADE_REPORT_CODE_UPGRADE_FAILED 104
+#define UPGRADE_REPORT_CODE_DEVICE_LOGIN 200
 
 #ifndef TEMP_FAILURE_RETRY
 #define TEMP_FAILURE_RETRY(exp)                \
@@ -101,6 +107,15 @@ int memory_buffer_reset(struct _memory_buffer *buf) {
 
     return 0;
 }
+
+static size_t _data_receive_dummy(void *ptr, size_t size, size_t nmemb,
+                                  void *userdata) {
+    (void)ptr;
+    (void)userdata;
+    // drop all data
+    return size * nmemb;
+}
+
 static size_t _data_receive(void *ptr, size_t size, size_t nmemb,
                             void *userdata) {
     struct _memory_buffer *buf = (struct _memory_buffer *)userdata;
@@ -260,11 +275,75 @@ static int _detect_response_parse(struct hrupdate_desc *desc, struct _memory_buf
     return 0;
 }
 
+static int _update_post_data(const char *url, const char *data) {
+    long response_code = 0;
+    CURL *curl = NULL;
+
+    curl = curl_easy_init();
+    if (!curl) return -1;
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_POST, 1L);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(data));
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 10L);
+    curl_easy_setopt(curl, CURLOPT_SERVER_RESPONSE_TIMEOUT, 60L);
+
+    // drop all response data
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _data_receive_dummy);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
+
+    CURLcode code = curl_easy_perform(curl);
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+    curl_easy_cleanup(curl);
+
+    if (CURLE_OK != code) {
+        printf("request failed ... %d:%s\n", code, curl_easy_strerror(code));
+        return -1;
+    }
+
+    if (response_code != 200) return -1;
+
+    return 0;
+}
+
+static int _update_report(int status) {
+    const char *id = "LC19013912001543";
+    const char *version = "1.1.3";
+    const char *fmt = "<SendInfo id=\"%s\" statuscode=\"%d\" version=\"%s\"/>";
+
+    const char *url = "https://msr.inspur.com:8011/UpgradeStatus";
+
+    char data[1024] = {0};
+
+    snprintf(data, sizeof(data), fmt, id, status, version);
+
+    return _update_post_data(url, data);
+}
+
+static int _device_bootup_event() {
+    const char *id = "LC19013912001543";
+    const char *ip = "1.11.1.1";
+    const char *mac = "08:10:74:00:00:01";
+    const char *version = "1.1.3";
+    const char *fmt = "<StartupInfo id=\"%s\" ip1=\"%s\" ip2=\"\" mac1=\"%s\" mac2=\"\" version=\"%s\" board=\"\"/>";
+
+    const char *url = "https://msr.inspur.com:8011/StartupInfo";
+
+    char data[1024] = {0};
+
+    snprintf(data, sizeof(data), fmt, id, ip, mac, version);
+
+    return _update_post_data(url, data);
+}
+
 static int _update_detect(struct hrupdate_desc *desc) {
     long response_code = 0;
     CURL *curl = NULL;
 
-    const char *url = "https://msr.inspur.com:8011/getPolicyFile?type=upgrade&deviceType=V352F_DMO1_FJYZ_MSR2023&version=1.1.1&ip=1.1.1.1&sn=LCRT1234567890&mac=00:00:00:00:00:00&areaCode=&hwVer=&cpu=&manufacture=Inspur&hardwareVersion=&soid=20";
+    const char *url = "https://msr.inspur.com:8011/getPolicyFile?type=upgrade&deviceType=V352F_DMO1_FJYZ_MSR2023&version=1.1.0&ip=1.1.1.1&sn=LCRT1234567890&mac=00:00:00:00:00:00&areaCode=&hwVer=&cpu=&manufacture=Inspur&hardwareVersion=&soid=20";
     // const char *url = "https://msr.inspur.com:8011/getPolicyFile?type=upgrade&deviceType=V352F_DMO1_FJYZ_MSR2023&version=1.1.3&ip=1.1.1.1&sn=LCRT1234567890&mac=00:00:00:00:00:00&areaCode=&hwVer=&cpu=&manufacture=Inspur&hardwareVersion=&soid=20";
 
     // const char *url = "http://www.baidu.com";
@@ -314,13 +393,13 @@ static int _update_download(struct hrupdate *update) {
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _download_receive);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, update);
 
-    printf("start downloading :%s ...\n", update->desc.url);
+    printf("hrupdate downloading :%s ...\n", update->desc.url);
     CURLcode code = curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
     curl_easy_cleanup(curl);
 
     if (CURLE_OK != code || response_code != 200) {
-        printf("request failed ... %d:%s\n", code, curl_easy_strerror(code));
+        printf("hrupdate request failed ... %d:%s\n", code, curl_easy_strerror(code));
 
         if (update->fd > 0) {
             close(update->fd);
@@ -364,12 +443,12 @@ static int _update_download(struct hrupdate *update) {
     // printf("md5:%s ...........\n", md5sum);
 
     if (update->desc.md5[0] != '\0') {
-       if (strncasecmp(md5sum, update->desc.md5, strlen(update->desc.md5)) != 0) {
-           printf("md5 not match!\n");
-           // delete file
-           unlink(update->path);
-           return -1;
-       }
+        if (strncasecmp(md5sum, update->desc.md5, strlen(update->desc.md5)) != 0) {
+            printf("md5 not match!\n");
+            // delete file
+            unlink(update->path);
+            return -1;
+        }
     } else {
         printf("ignore md5 verify\n");
     }
@@ -382,18 +461,20 @@ int main(int argc, char **argv) {
     int ret = 0;
     int ch = 0;
     int detect_only = 0;
-    int detect_skip = 0;
+    int local_package = 0;
+    int reboot = 0;
+    char cwd[PATH_MAX] = {0};
 
     struct hrupdate update = {.fd = -1, .desc = {{0}, 0, {0}, {0}}};
 
     memset((void *)&update, 0, sizeof(update));
     update.fd = -1;
-    snprintf(update.path, sizeof(update.path), "%s", "update.bin");
+    snprintf(update.path, sizeof(update.path), "%s/%s", getcwd(cwd, sizeof(cwd)), "update.bin");
 
     // -d: special detect detect only
     // -o: special output file
     // -l: using manual direct download url
-    while ((ch = getopt(argc, argv, "do:l:")) != -1) {
+    while ((ch = getopt(argc, argv, "do:l:r")) != -1) {
         switch (ch) {
             case 'd':
                 detect_only = 1;
@@ -402,8 +483,11 @@ int main(int argc, char **argv) {
                 snprintf(update.path, sizeof(update.path), "%s", optarg);
                 break;
             case 'l':
-                detect_skip = 1;
+                local_package = 1;
                 snprintf(update.desc.url, sizeof(update.desc.url), "%s", optarg);
+                break;
+            case 'r':
+                reboot = 1;
                 break;
             default:
                 printf("not support!\n");
@@ -411,28 +495,50 @@ int main(int argc, char **argv) {
         }
     }
 
+    // using line buffer, so popen can read output realtime
+    setvbuf(stdout, NULL, _IOLBF, 0);
+
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
-    if (detect_skip != 1) {
-       memory_buffer_alloc(&detect_data, 2048);
-       ret = _update_detect(&update.desc);
-       memory_buffer_free(&detect_data);
-    
-       if (ret < 0) return -1;
-    
-       printf("desc: size:%ld, md5sum:%s, url:%s\n", update.desc.size, update.desc.md5, update.desc.url);
-    
-       if (detect_only == 1) {
-           return 0;
-       }
-    
-       if (update.desc.size == 0 || update.desc.md5[0] == '\0' || update.desc.url[0] == '\0') {
-           printf("no valid update description ...\n");
-           return -1;
-       }
+    if (local_package != 1) {
+        memory_buffer_alloc(&detect_data, 2048);
+        ret = _update_detect(&update.desc);
+        memory_buffer_free(&detect_data);
+
+        if (ret < 0) return -1;
+
+        if (update.desc.size == 0 || update.desc.md5[0] == '\0' || update.desc.url[0] == '\0') {
+            printf("no need upgrade\n");
+            return 0;
+        }
+
+        printf("find new package:\n");
+        printf("version:%s\n", update.desc.version);
+        printf("url:%s\n", update.desc.url);
+        printf("size:%ld\n", update.desc.size);
+        printf("md5:%s\n", update.desc.md5);
+
+        if (detect_only == 1) {
+            return 0;
+        }
     }
 
-    ret = _update_download(&update);
+    if (local_package != 1)
+        _update_report(UPGRADE_REPORT_CODE_DOWNLOADING);  // start downloading
 
+    ret = _update_download(&update);
+    if (ret != 0) {
+        if (local_package != 1)
+            _update_report(UPGRADE_REPORT_CODE_DOWNLOAD_FAILED);
+        return -1;
+    }
+    if (local_package != 1)
+        _update_report(UPGRADE_REPORT_CODE_DOWNLOAD_SUCCESS);
+
+    // perform real upgrade
+    usleep(1000 * 1000);
+
+    if (local_package != 1)
+        _update_report(UPGRADE_REPORT_CODE_UPGRADE_SUCCESS);
     return 0;
 }
