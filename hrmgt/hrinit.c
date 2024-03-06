5e9ccfbbc941d4da34f78c66069fb48a87dbb00f
Alex Meng <mengxp@inspur.com>
2024-03-06 18:12:11 +0800

    init support property
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- hrmgt/hrinit.c
+++ hrmgt/hrinit.c
@@ -1,18 +1,17 @@
+#define _GNU_SOURCE
 #include <assert.h>
 #include <dirent.h>
-#include <dlfcn.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <limits.h>
-#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/epoll.h>
-#include <sys/inotify.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include "hr_list.h"
@@ -33,6 +32,8 @@
 
 #define DEFAULT_INIT_SERVICE_DIR "init.d"
 
+#define HRINIT_PROPERTY_SOCK "./var/property_sock"
+
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #endif
@@ -48,7 +49,16 @@
     })
 #endif
 
+#define IPC_MESSAGE_CMD_HEAD 0x12340000
+
+#define IPC_MESSAGE_CMD_ACK 0x12347890
+
+struct _hrinit {
+    int epoll_fd;
+    int property_sock;
+};
 static struct j2sobject *_service_list = NULL;
+static struct _hrinit _priv = {-1};
 
 // 获取元素 ele 在结构体中偏移量
 #define _J2SOBJECT_SERVICE_DATA_OFFSET(ele) \
@@ -70,7 +80,7 @@ typedef struct j2sobject_service {
     // action list
     // int nargs;
     char *argv[MAX_ARGS];  // max args
-    int flag; // append other not stored fields
+    int flag;              // append other not stored fields
 } j2sobject_service_t;
 
 static int j2sobject_service_ctor(struct j2sobject *obj);
@@ -99,41 +109,39 @@ static int j2sobject_service_ctor(struct j2sobject *obj) {
     return 0;
 }
 
-static int _load_service(const char *path) {
+static int _load_service(const char *directory) {
     DIR *dir = NULL;
     struct dirent *entry = NULL;
     struct stat st;
 
-    if (!path) return -1;
+    if (!directory) return -1;
 
-    if (stat(path, &st) || !S_ISDIR(st.st_mode)) {
+    if (stat(directory, &st) || !S_ISDIR(st.st_mode)) {
         HR_LOGE("it's not a good directory ...\n");
         return -1;
     }
 
-    dir = opendir(path);
+    dir = opendir(directory);
     if (!dir) {
         HR_LOGE("can not open directory ...\n");
         return -1;
     }
 
     while ((entry = readdir(dir))) {
-        void *handle = NULL;
-        char plugin_so[512] = {0};
+        char path[512] = {0};
         size_t len = 0;
 
         if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, "..")) continue;
 
         len = strlen(entry->d_name);
 
-        snprintf(plugin_so, sizeof(plugin_so), "%s/%s", path, entry->d_name);
-
-        printf("now path:%s\n", plugin_so);
+        snprintf(path, sizeof(path), "%s/%s", directory, entry->d_name);
 
         struct j2sobject_service *self = (struct j2sobject_service *)j2sobject_create(&_j2sobject_service_prototype);
 
-        int ret = j2sobject_deserialize_file(J2SOBJECT(self), plugin_so);
+        int ret = j2sobject_deserialize_file(J2SOBJECT(self), path);
         if (ret != 0) {
+            printf("parse %s failed\n", entry->d_name);
             j2sobject_free(J2SOBJECT(self));
             continue;
         }
@@ -151,27 +159,234 @@ static int _load_service(const char *path) {
         printf("\n");
         printf("+++++++++++++++++++++++++++\n");
         // insert into tail
-        J2SOBJECT(_service_list)->prev->next = J2SOBJECT(self);
-        J2SOBJECT(self)->prev = J2SOBJECT(_service_list)->prev;
-        J2SOBJECT(self)->next = J2SOBJECT(_service_list);
-        J2SOBJECT(_service_list)->prev = J2SOBJECT(self);
+        _service_list->prev->next = J2SOBJECT(self);
+        J2SOBJECT(self)->prev = _service_list->prev;
+        J2SOBJECT(self)->next = _service_list;
+        _service_list->prev = J2SOBJECT(self);
     }
 
     closedir(dir);
     return 0;
 }
 
+static int _property_sock() {
+    int ret = -1;
+    int type = SOCK_STREAM | SOCK_CLOEXEC | SOCK_NONBLOCK;
+    struct sockaddr_un addr;
+
+    int fd = socket(AF_UNIX, type, 0);
+    if (fd < 0) return -1;
+
+    fcntl(fd, F_SETFD, FD_CLOEXEC);
+    fcntl(fd, F_SETFL, O_NONBLOCK);
+
+    memset((void *)&addr, 0, sizeof(addr));
+
+    addr.sun_family = AF_UNIX;
+    snprintf(addr.sun_path, sizeof(addr.sun_path), "%s", HRINIT_PROPERTY_SOCK);
+
+    if ((unlink(addr.sun_path) != 0) && (errno != ENOENT)) {
+        printf("can not unlink old file\n");
+        return -1;
+    }
+    ret = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret != 0) {
+        unlink(addr.sun_path);
+        close(fd);
+        printf("call bind error :%s ...\n", strerror(errno));
+        return -1;
+    }
+
+    listen(fd, 8);
+    return fd;
+}
+
+static int _recv_fully(int fd, void *data_ptr, size_t size) {
+    size_t bytes_left = size;
+    char *data = (char *)data_ptr;
+
+    if (fd < 0) return -1;
+    while (bytes_left > 0) {
+        int result =
+            TEMP_FAILURE_RETRY(recv(fd, data, bytes_left, MSG_DONTWAIT));
+        if (result <= 0) {
+            return -1;
+        }
+
+        bytes_left -= result;
+        data += result;
+    }
+
+    return bytes_left == 0 ? 0 : -1;
+}
+
+static char *_recv_string(int fd) {
+    int ret = 0;
+    int len = 0;
+    void *ptr = NULL;
+    ret = _recv_fully(fd, (void *)&len, sizeof(len));
+    if (ret != 0) {
+        printf("read data error ...size.\n");
+        return NULL;
+    }
+
+    printf("len:%d\n", len);
+    ptr = calloc(1, len);
+    if (!ptr) {
+        // can not alloc memory, but we should read all data
+        int i = 0;
+        for (i = 0; i < len; i++) {
+            char data = 0;
+            _recv_fully(fd, (void *)&data, sizeof(data));
+        }
+        // TEMP_FAILURE_RETRY(send(fd, &reply, sizeof(reply), 0));
+        return NULL;
+    }
+
+    // data
+    ret = _recv_fully(fd, (void *)ptr, len);
+
+    return ptr;
+}
+static int _message_available(int fd) {
+    // int val = 0;
+    int cmd = 0;
+    int len = 0;
+    int reply = IPC_MESSAGE_CMD_ACK;
+    void *ptr = NULL;
+
+    if (fd < 0) {
+        printf("_data_available: invalid fd:%d\n", fd);
+        return -1;
+    }
+
+    int ret = _recv_fully(fd, (void *)&cmd, sizeof(cmd));
+    if (ret != 0) {
+        printf("read data error ...cmd .\n");
+        return -1;
+    }
+
+    printf("cmd:0x%X\n", cmd);
+    if ((cmd & 0xFFFF0000) != IPC_MESSAGE_CMD_HEAD) {
+        printf("invalid header ignore ...\n");
+        return -1;
+    }
+
+#if 0
+    // read full data size
+    ret = _recv_fully(fd, (void *)&len, sizeof(len));
+    if (ret != 0) {
+        printf("read data error ...size.\n");
+        return -1;
+    }
+
+    printf("len:%d\n", len);
+    ptr = calloc(1, len);
+    if (!ptr) {
+        // can not alloc memory, but we should read all data
+        int i = 0;
+        for (i = 0; i < len; i++) {
+            char data = 0;
+            _recv_fully(fd, (void *)&data, sizeof(data));
+        }
+        TEMP_FAILURE_RETRY(send(fd, &reply, sizeof(reply), 0));
+        return -1;
+    }
+
+    // data
+    ret = _recv_fully(fd, (void *)ptr, len);
+
+    if (ret != 0) {
+        free(ptr);
+        return -1;
+    }
+#endif
+    ptr = _recv_string(fd);
+    printf("name:%s\n", (char *)ptr);
+    free(ptr);
+
+    ptr = _recv_string(fd);
+    printf("val:%s\n", (char *)ptr);
+    free(ptr);
+    // send ack
+    TEMP_FAILURE_RETRY(send(fd, &reply, sizeof(reply), 0));
+
+    return 0;
+}
 int main(int argc, const char **argv) {
     (void)argc;
     (void)argv;
+    struct epoll_event ev;
+    struct epoll_event evs[24];
 
+    _priv.epoll_fd = epoll_create1(O_CLOEXEC);
+    if (_priv.epoll_fd < 0)
+        return -1;
+
+    printf("epoll:%d\n", _priv.epoll_fd);
+    _priv.property_sock = _property_sock();
+    if (_priv.property_sock < 0) {
+        close(_priv.epoll_fd);
+        return -1;
+    }
+
+    printf("sock:%d\n", _priv.property_sock);
     _service_list = j2sobject_create_array(&_j2sobject_service_prototype);
     assert(_service_list);
 
     _load_service(DEFAULT_INIT_SERVICE_DIR);
 
+    memset((void *)&ev, 0, sizeof(ev));
+    ev.events = EPOLLIN;
+    ev.data.fd = _priv.property_sock;
+    epoll_ctl(_priv.epoll_fd, EPOLL_CTL_ADD, _priv.property_sock, &ev);
+
+    for (;;) {
+        int timeout = -1;  // 12000;
+
+        int nr = TEMP_FAILURE_RETRY(epoll_wait(_priv.epoll_fd, evs, ARRAY_SIZE(evs), timeout));
+        if (nr == 0 || nr == -1) {
+            // timeout
+            printf("maybe timeout ...\n");
+            continue;
+        }
+
+        for (int i = 0; i < nr; i++) {
+            struct epoll_event *e = evs + i;
+            printf("%d vs %d is ready!\n", e->data.fd, _priv.property_sock);
+            // new connection ?
+            if (_priv.property_sock == e->data.fd) {
+                int fd = accept4(e->data.fd, NULL, NULL, SOCK_CLOEXEC);
+                if (fd < 0) {
+                    continue;
+                }
+
+                printf("fd:%d\n", fd);
+                memset((void *)&ev, 0, sizeof(ev));
+                ev.events = EPOLLIN;
+                ev.data.fd = fd;
+                if (epoll_ctl(_priv.epoll_fd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+                    close(fd);
+                    continue;
+                }
+            } else {
+                // we should read stream
+                printf("please read all stream ...%d\n", e->data.fd);
+                int ret = _message_available(e->data.fd);
+                if (ret < 0) {
+                    // we should remove fd from epoll
+                    epoll_ctl(_priv.epoll_fd, EPOLL_CTL_DEL, ev.data.fd, NULL);
+                    close(ev.data.fd);
+                }
+                sleep(2);
+                // exit(1);
+            }
+        }
+    }
     // all elements on the link will be freed
     j2sobject_free(_service_list);
     _service_list = NULL;
+    close(_priv.epoll_fd);
+    _priv.epoll_fd = -1;
     return 0;
 }
---
+++ hrmgt/hrinit_control.c
@@ -0,0 +1,187 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+// please make sure all macro same with inspur's code
+// mxp, 20231030, notify event to ahsapd channel
+#define HRINIT_PROPERTY_SOCK "./var/property_sock"
+
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(exp)                \
+    ({                                         \
+        typeof(exp) _rc;                       \
+        do {                                   \
+            _rc = (exp);                       \
+        } while (_rc == -1 && errno == EINTR); \
+        _rc;                                   \
+    })
+#endif
+
+#define IPC_MESSAGE_CMD_HEAD 0x12340000
+
+#define IPC_MESSAGE_CMD_ACK 0x12347890
+
+#define IPC_MESSAGE_CMD_LANHOST (IPC_MESSAGE_CMD_HEAD | 0x01)
+
+static int _ipc_channel_fd = -1;
+
+static int _poll_in(int fd, int timeout) {
+    int nr = 0;
+    struct pollfd ufds[1];
+    ufds[0].fd = fd;
+    ufds[0].events = POLLIN;
+    ufds[0].revents = 0;
+
+    nr = poll(ufds, 1, timeout);
+
+    return nr;
+}
+
+// not readable direct return
+// readable but read fail socket is bad
+// 1: connected
+// other: non connected
+static int _sock_is_connected(int fd) {
+    if (fd < 0) {
+        return 0;
+    }
+    if (1 == _poll_in(fd, 0)) {
+        char c = 0;
+        if (recv(fd, &c, 1, MSG_PEEK) <= 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+static int _ipc_channel_init_ensured() {
+    int ret = -1;
+    int type = SOCK_STREAM | SOCK_CLOEXEC;
+    struct sockaddr_un addr;
+    int fd = -1;
+
+    if (_ipc_channel_fd != -1) {
+        // printf("already connected .. ...\n");
+        return 0;
+    }
+
+    fd = socket(AF_UNIX, type, 0);
+    if (fd < 0) return -1;
+
+    fcntl(fd, F_SETFD, FD_CLOEXEC);
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+
+    memset((void *)&addr, 0, sizeof(addr));
+
+    addr.sun_family = AF_UNIX;
+    // or strcpy
+    snprintf(addr.sun_path, sizeof(addr.sun_path), "%s", HRINIT_PROPERTY_SOCK);
+
+    // maybe failed because ahsapd may not startup
+    ret = TEMP_FAILURE_RETRY(
+        connect(fd, (const struct sockaddr *)&addr, sizeof(addr)));
+    printf("connect:%d\n", ret);
+    if (ret != 0) {
+        close(fd);
+        // printf("can not connect ....remote ...\n");
+        return -1;
+    }
+
+    _ipc_channel_fd = fd;
+
+    return 0;
+}
+static int _recv_fully(int fd, void *data_ptr, size_t size) {
+    size_t bytes_left = size;
+    char *data = (char *)data_ptr;
+
+    if (fd < 0) return -1;
+    while (bytes_left > 0) {
+        // error or timeout
+        if (_poll_in(fd, 5000) <= 0) {
+            return -1;
+        }
+
+        int result = TEMP_FAILURE_RETRY(recv(fd, data, bytes_left, /*MSG_WAITALL*/ MSG_DONTWAIT));
+        if (result <= 0) {
+            return -1;
+        }
+
+        bytes_left -= result;
+        data += result;
+    }
+
+    return bytes_left == 0 ? 0 : -1;
+}
+static int _send_string(const char *val) {
+    int ret = -1;
+    int len = 0;
+    len = strlen(val);
+    ret = TEMP_FAILURE_RETRY(send(_ipc_channel_fd, (const void *)&len, sizeof(len), 0));
+    if (ret < 0) {
+        return ret;
+    }
+    ret = TEMP_FAILURE_RETRY(send(_ipc_channel_fd, (const void *)val, len, 0));
+    if (ret < 0) {
+        return ret;
+    }
+
+    return 0;
+}
+static int hrinit_control(const char *name, const char *val) {
+    int ret = -1;
+    int length = 0;
+    int cmd = IPC_MESSAGE_CMD_HEAD;
+    int ack = -1;
+    if (_ipc_channel_fd != -1) {
+        if (1 != _sock_is_connected(_ipc_channel_fd)) {  // broken ?
+            close(_ipc_channel_fd);
+            _ipc_channel_fd = -1;
+        }
+    }
+
+    if (_ipc_channel_init_ensured() != 0) {
+        return -1;
+    }
+
+    ret = TEMP_FAILURE_RETRY(send(_ipc_channel_fd, (const void *)&cmd, sizeof(cmd), 0));
+    if (ret < 0) {
+        goto error;
+    }
+
+    // send name
+    _send_string(name);
+    // send value
+    _send_string(val);
+
+    ret = _recv_fully(_ipc_channel_fd, &ack, sizeof(ack));
+    if (ret != 0) {
+        goto error;
+    }
+
+    return 0;
+error:
+    close(_ipc_channel_fd);
+    _ipc_channel_fd = -1;
+    return -1;
+}
+int main(int argc, char **argv) {
+    hrinit_control("ctrl.start", "hrupdate");
+    getchar();
+    hrinit_control("ctrl.stop", "hrupdate");
+    getchar();
+    close(_ipc_channel_fd);
+    return 0;
+}
