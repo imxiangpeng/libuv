8c3f4f553daebd0ab79b91907b80faf2683e2ee1
Alex Meng <mengxp@inspur.com>
2024-03-13 11:09:05 +0800

    http post file process multi part
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- hrmgt/http_postfile.c
+++ hrmgt/http_postfile.c
@@ -50,7 +50,11 @@ int _memline(char **ptr, size_t *size, void *data, size_t max) {
 
 #endif
     len = _memcontext(data, max);
-    len += 2; // skip \r\n
+    if (len == max) {
+        // data not enough ...
+    } else {
+        len += 2;  // skip \r\n
+    }
     printf("len:%d\n", len);
     if (*size < len + 1) {
         *ptr = realloc(*ptr, len + 1);
@@ -58,6 +62,7 @@ int _memline(char **ptr, size_t *size, void *data, size_t max) {
     }
 
     memcpy(*ptr, data, len);
+    // strncat(*ptr, data, len);
     // *(*ptr-1) = '\0';
 
     *(*ptr + len) = '\0';
@@ -70,7 +75,8 @@ int main(int argc, char **argv) {
     char *buf = calloc(1, BUF_SIZE);
     int idx = 0;
 
-    const char *boundary = "----WebKitFormBoundaryTmNn7iRszae1RJ8N";
+    // const char *boundary = "----WebKitFormBoundaryTmNn7iRszae1RJ8N";
+    const char *boundary = "----WebKitFormBoundaryLHRXX18JscMKZPMw";
 
     const char *save = "real.bin";
     int fd = open(save, O_CREAT | O_TRUNC | O_RDWR, 0644);
@@ -86,27 +92,7 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-#if 0
-    size_t s = 512;
-    char* line = (char*)calloc(1, s);
-    printf("line:%p\n", line);
-    int i = 0;
-    while (!feof(rfp)) {
-        int ret = getline(&line, &s, rfp);
-        i++;
-        if (i > 4) break;
-        printf("line -> (%ld):%s\n", strlen(line), line);
-        printf("line:");
-        for (int i = 0; i < strlen(line); i++) {
-            printf("0x%X ", line[i]);
-        }
-        printf("\n");
-    }
-    printf("line:%p, size:%ld\n", line, s);
-    free(line);
-#endif
-
-    int state = S_INIT;
+    int state = S_BOUNDARY;
     size_t l = 0;
     size_t reserve = 0;
     size_t line_max = 1024;
@@ -115,6 +101,9 @@ int main(int argc, char **argv) {
     int b = 0;
     while (!feof(rfp) || reserve != 0) {
         printf("try read :%ld(%ld)\n", BUF_SIZE - reserve, reserve);
+
+    readdata:
+
         size_t ss = fread(buf + reserve, 1, BUF_SIZE - reserve, rfp);
         printf("read :%ld\n", ss);
         ss += reserve;
@@ -123,72 +112,73 @@ int main(int argc, char **argv) {
         // rewind
         l = 0;
 
+        if (ss == 0) {
+            printf("no data anymore ...\n");
+            break;
+        }
+
         while (l < ss) {
             printf("state:%d, l:%ld\n", state, l);
             switch (state) {
-                case S_INIT: {
-                    if (buf[l] != '-' || buf[l + 1] != '-') {
-                        l++;
-                        continue;
-                    }
-                    l += 2;  // skip --
-                    state = S_BOUNDARY;
-                    break;
-                }
                 case S_BOUNDARY: {  // search boundary
-                    // verify new data
-                    printf("buf +l:%s vs %s\n", buf + l, boundary);
-                    if (0 == strstr(buf + l, boundary)) {
-                        printf("no more part!\n");
+                    // our line is large enough if -- begin
+
+                    if (buf[l] != '-' || buf[l + 1] != '-') {
+                        printf("boundary error not expected data \n");
                         return -1;
                     }
+                    l += 2;
+
+                    if (memcmp((void *)buf + l, boundary, strlen(boundary)) != 0) {
+                        printf("boundary error not expected data, found -- but not boundary ...\n");
+                        return -1;
+                    }
+                    // verify new data
+                    printf("buf +l:%s vs %s\n", buf + l, boundary);
+
                     l += strlen(boundary);
+                    if (buf[l] == '-' && buf[l + 1] == '-') {
+                        printf("all boundary end ...\n");
+                        return 0;
+                    }
                     l += 2;  // skip \r\n
                     state = S_CTX_DISP;
                     break;
                 }
-                case S_CTX_DISP: {
+                case S_CTX_DISP: {  // metadata
+                    int ret = 0;
                     memset((void *)line, 0, line_max);
-                    int ret = _memline(&line, &line_max, buf + l, ss - l);
-                    printf("read line:%d -> %s\n", ret, line);
-
-                    char *p = strstr(line, "Content-Disposition:");
-                    if (!p) {
-                        printf("%s(%d): ...invalid content disposition.....\n", __FUNCTION__,
-                               __LINE__);
-                        return -01;
+                    while (l < ss && ((ret = _memline(&line, &line_max, buf + l, ss - l)) != 2)) {
+                        printf("read line:ss -l: %ld vs %d -> %s\n", ss - l, ret, line);
+                        if (ret == ss - l) {
+                            printf("data not enough, read more ...\n");
+                            // not found line end, data is not enough!
+                            // memcpy(buf, buf + l, ss - l);
+                            // reserve = ss - l;
+                            // ss = 0;
+                            // goto readdata;
+                        }
+                        l += ret;
+                        memset((void *)line, 0, line_max);
                     }
-                    char *form = p + strlen("Content-Disposition:") + 1;
-                    printf("form:%s\n", form);
-                    l += ret;
-                    state = S_CTX_TYPE;
-                    break;
-                }
-                case S_CTX_TYPE: {
-                    memset((void *)line, 0, line_max);
-                    int ret = _memline(&line, &line_max, buf + l, ss - l);
-                    printf("read line:%d -> %s\n", ret, line);
-                    l += ret;
-                    if (ret == 2) {
-                        state = S_CTX_PAYLOAD;
-                    } else {
-                        state = S_BOUNDARY_END;
-                    }
-                    break;
-                }
-                case S_BOUNDARY_END: {
 
-                    memset((void *)line, 0, line_max);
-                    int ret = _memline(&line, &line_max, buf + l, ss - l);
-                    printf("bound end line:%d -> %s\n", ret, line);
-                    if (buf[l] != '\r' && buf[l + 1] != '\n')
-                        return -1;
-                    l += 2;
+                    printf("l:%ld, ss:%ld, ret:%d\n", l, ss, ret);
+                    if (l >= ss) {
+                        printf("%s(%d): maybe error data: %ld vs %ld\n", __FUNCTION__, __LINE__, l, ss);
+                        // return -1;
+                        goto readdata;
+                    }
+
+                    // meet empty \r\n
+                    l += 2;  // skip empty \r\n
+
                     state = S_CTX_PAYLOAD;
+
                     break;
                 }
 
                 case S_CTX_PAYLOAD: {
+                    // return data length before \r\n, not contains \r\n
                     ssize_t available = _memcontext((void *)(buf + l), ss - l);
                     if (available > 0) {
                         ssize_t s = write(fd, (void *)(buf + l), available);
@@ -202,12 +192,14 @@ int main(int argc, char **argv) {
                     } else {
                         printf("found '\\r\\n', stopped ?\n");
                         char *ptr = buf + l;
-                        // *ptr == '\r';
-                        // *(ptr + 1) == '\n';
+                        // assert *ptr == '\r';
+                        // assert *(ptr + 1) == '\n';
                         if (*(ptr + 2) == '-' && *(ptr + 3) == '-') {
                             if (memcmp(ptr + 4, boundary, strlen(boundary)) == 0) {
-                                printf("real stop ...\n");
-                                return 0;
+                                printf("real stop .or next trunck..\n");
+                                l += 2;  // skip \r\n
+                                state = S_BOUNDARY;
+                                break;
                             }
                         } else {
                             ssize_t s = write(fd, (void *)(buf + l), 2 + 2);
